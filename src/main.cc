d995bc03074283d5f9bf8c847aa0ef7a3e4623a1
Alec Roelke <ar4jc@virginia.edu>
2017-11-20 19:05:02 -0500

    Move most things out of the cmd parse try block

--- src/main.cc
+++ src/main.cc
@@ -63,68 +63,25 @@ int main(int argc, char* argv[])
 {
     using namespace TCLAP;
 
-    int n;
-    bool v, rates;
-    string time_units, dist_file;
+    vector<string> time_units{"seconds", "minutes", "hours", "days", "weeks", "months", "years"};
+    ValuesConstraint<string> time_constraint(time_units);
+
     vector<shared_ptr<FailureMechanism>> mechanisms;
     xml_document doc;
 
+    CmdLine cmd("Compute the reliability distribution of a chip", ' ', "0.1");
+    SwitchArg verbose("v", "verbose", "Display progress output", cmd);
+    SwitchArg separate("", "separate-aging-rates", "Display a second table with aging rates separated by mechanism per unit (only works for fresh configuration)", cmd);
+    ValueArg<string> phenomena("", "aging-mechanisms", "Comma-separated list of aging mechanisms to include or \"all\" for all of them", false, "all", "mechanisms", cmd);
+    ValueArg<char> delimiter("", "trace-delimiter", "One-character delimiter for data in input trace files (default: ,)", false, ',', "delim", cmd);
+    ValueArg<string> time("", "time-units", "Units for displaying time to failure (default: hours)", false, "hours", &time_constraint, cmd);
+    ValueArg<int> iterations("n", "iterations", "Number of Monte-Carlo iterations to perform (default: 1000)", false, 1000, "iterations", cmd);
+    ValueArg<string> dist_dump("", "dump-ttfs", "Dump time-to-failure distribution to file", false, "", "filename", cmd);
+    UnlabeledValueArg<string> config("chip-config", "File containing chip configuration", true, "", "filename", cmd);
+
     try
     {
-        vector<string> units{"seconds", "minutes", "hours", "days", "weeks", "months", "years"};
-        ValuesConstraint<string> unit_values(units);
-
-        CmdLine cmd("Compute the reliability distribution of a chip", ' ', "0.1");
-        SwitchArg verbose("v", "verbose", "Display progress output", cmd);
-        SwitchArg separate("", "separate-aging-rates", "Display a second table with aging rates separated by mechanism per unit (only works for fresh configuration)", cmd);
-        ValueArg<string> phenomena("", "aging-mechanisms", "Comma-separated list of aging mechanisms to include or \"all\" for all of them", false, "all", "mechanisms", cmd);
-        ValueArg<char> delimiter("", "trace-delimiter", "One-character delimiter for data in input trace files (default: ,)", false, ',', "delim", cmd);
-        ValueArg<string> time("", "time-units", "Units for displaying time to failure (default: hours)", false, "hours", &unit_values, cmd);
-        ValueArg<int> iterations("n", "iterations", "Number of Monte-Carlo iterations to perform (default: 1000)", false, 1000, "iterations", cmd);
-        ValueArg<string> dist_dump("", "dump-ttfs", "Dump time-to-failure distribution to file", false, "", "filename", cmd);
-        UnlabeledValueArg<string> config("chip-config", "File containing chip configuration", true, "", "filename", cmd);
         cmd.parse(argc, argv);
-
-        xml_parse_result result = doc.load_file(config.getValue().c_str());
-        if (!result)
-        {
-            cerr << config.getValue() << ": " << result.description()
-                 << " at offset " << result.offset << endl;
-            return 1;
-        }
-
-        Unit::delim = delimiter.getValue();
-        n = iterations.getValue();
-        time_units = time.getValue();
-        dist_file = dist_dump.getValue();
-        rates = separate.getValue();
-        v = verbose.getValue();
-
-        string p = phenomena.getValue();
-        transform(p.begin(), p.end(), p.begin(), ::tolower);
-        if (p == "all")
-            mechanisms = {NBTI::model(), EM::model(), HCI::model(), TDDB::model()};
-        else
-        {
-            for (const string& token: split(p, ','))
-            {
-                if (token == "nbti")
-                    mechanisms.push_back(NBTI::model());
-                else if (token == "em")
-                    mechanisms.push_back(EM::model());
-                else if (token == "hci")
-                    mechanisms.push_back(HCI::model());
-                else if (token == "tddb")
-                    mechanisms.push_back(TDDB::model());
-                else
-                    cerr << "warning: ignoring unknown aging mechanism \"" << token << '"' << endl;
-            }
-        }
-        if (mechanisms.empty())
-        {
-            cerr << "error: no aging mechanisms selected" << endl;
-            return 1;
-        }
     }
     catch (ArgException& e)
     {
@@ -132,7 +89,43 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    if (v)
+    xml_parse_result result = doc.load_file(config.getValue().c_str());
+    if (!result)
+    {
+        cerr << config.getValue() << ": " << result.description()
+                << " at offset " << result.offset << endl;
+        return 1;
+    }
+
+    Unit::delim = delimiter.getValue();
+
+    string p = phenomena.getValue();
+    transform(p.begin(), p.end(), p.begin(), ::tolower);
+    if (p == "all")
+        mechanisms = {NBTI::model(), EM::model(), HCI::model(), TDDB::model()};
+    else
+    {
+        for (const string& token: split(p, ','))
+        {
+            if (token == "nbti")
+                mechanisms.push_back(NBTI::model());
+            else if (token == "em")
+                mechanisms.push_back(EM::model());
+            else if (token == "hci")
+                mechanisms.push_back(HCI::model());
+            else if (token == "tddb")
+                mechanisms.push_back(TDDB::model());
+            else
+                cerr << "warning: ignoring unknown aging mechanism \"" << token << '"' << endl;
+        }
+    }
+    if (mechanisms.empty())
+    {
+        cerr << "error: no aging mechanisms selected" << endl;
+        return 1;
+    }
+
+    if (verbose.getValue())
         cout << "Creating units..." << endl;
     vector<shared_ptr<Unit>> units;
     for (const xml_node& child: doc.children("unit"))
@@ -151,19 +144,19 @@ int main(int argc, char* argv[])
             exit(1);
         }
     }
-    if (v)
+    if (verbose.getValue())
         cout << "Creating failure dependency graph..." << endl;
     shared_ptr<Component> root = make_shared<Group>(doc.child("group"), units);
 
-    if (v)
+    if (verbose.getValue())
         cout << "Computing aging rates..." << endl;
     for (const shared_ptr<Unit>& unit: units)
         unit->compute_reliability(mechanisms);
 
     // Monte Carlo sim to get overall failure distribution
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < iterations.getValue(); i++)
     {
-        if (v)
+        if (verbose.getValue())
             cout << "Beginning Monte Carlo iteration " << i << endl;
 
         unordered_set<shared_ptr<Component>> failed_components;
@@ -218,18 +211,18 @@ int main(int argc, char* argv[])
     vector<string> cols{"MTTF", "Failures", "Aging Rate"};
     transform(units.begin(), units.end(), back_inserter(rows), [](const shared_ptr<Unit>& u){ return u->name; });
     unordered_map<string, unordered_map<string, double>> data;
-    data[root->name]["MTTF"] = convert_time(root->mttf(), time_units);
+    data[root->name]["MTTF"] = convert_time(root->mttf(), time.getValue());
     data[root->name]["Failures"] = root->ttfs.size();
     data[root->name]["Aging Rate"] = numeric_limits<double>::quiet_NaN();
     for (const shared_ptr<Unit>& u: units)
     {
-        data[u->name]["MTTF"] = convert_time(u->mttf(), time_units);
+        data[u->name]["MTTF"] = convert_time(u->mttf(), time.getValue());
         data[u->name]["Failures"] = u->ttfs.size();
-        data[u->name]["Aging Rate"] = convert_time(u->aging_rate(Unit::fresh), time_units);
+        data[u->name]["Aging Rate"] = convert_time(u->aging_rate(Unit::fresh), time.getValue());
     }
     print_table(rows, cols, data);
 
-    if (rates)
+    if (separate.getValue())
     {
         sort(units.begin(), units.end(), [&](const shared_ptr<Unit>& a, const shared_ptr<Unit>& b) {
             return b->aging_rate(Unit::fresh) < a->aging_rate(Unit::fresh);
@@ -241,14 +234,14 @@ int main(int argc, char* argv[])
         data.clear();
         for (const shared_ptr<Unit>& u: units)
             for (const shared_ptr<FailureMechanism>& m: mechanisms)
-                data[u->name][m->name] = convert_time(u->aging_rate(m), time_units);
+                data[u->name][m->name] = convert_time(u->aging_rate(m), time.getValue());
         cout << endl;
         print_table(rows, cols, data);
     }
 
-    if (!dist_file.empty())
+    if (!dist_dump.getValue().empty())
     {
-        ofstream dist(dist_file);
+        ofstream dist(dist_dump.getValue());
         if (dist)
         {
             dist << root->name;
